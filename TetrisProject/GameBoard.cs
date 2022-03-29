22d27b6bc89e84ea8b2891ac942d521dafe653ae
daniwik662 <daniwik662@student.polsl.pl>
2022-03-29 20:44:20 +0200

    I have created Gameboard class + added array for the IDs of the blosk

---
+++ TetrisProject/GameBoard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisProject
+{
+    class GameBoard
+    {
+        public int[,] grid = new int[22, 10];
+    }
+}
