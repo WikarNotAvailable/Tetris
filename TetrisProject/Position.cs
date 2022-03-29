97db09d6f308a213d40329605c3e8c31928ded39
daniwik662 <daniwik662@student.polsl.pl>
2022-03-29 20:39:50 +0200

    Position class (used for tracking places of blocks on a gameboard)

---
+++ TetrisProject/Position.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisProject
+{
+    public class Position
+    {
+        public int column;
+        public int row;
+        public Position(int x, int y) 
+        {
+            column = y;
+            row = x;
+        }
+    }
+}
