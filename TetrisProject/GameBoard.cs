106f16c0963e489028d952696d0e5a034a0b8dd1
daniwik662 <daniwik662@student.polsl.pl>
2022-03-30 16:29:26 +0200

    added constructor to GameBoard class which is also filling the grid with 0 and overloaded index operator

--- TetrisProject/GameBoard.cs
+++ TetrisProject/GameBoard.cs
@@ -8,8 +8,27 @@ namespace TetrisProject
 {
     class GameBoard
     {
-        public int[,] grid = new int[22, 10];
+        public int[,] grid; 
 
+        public GameBoard()
+        {
+            grid = new int[22, 10];
 
+            for (int r=0; r< 22; r++)
+            {
+                for (int c = 0; c < 10; c++)
+                {
+                    grid[r,c] = 0;
+                }
+            }
+        }
+
+        public int this [int row, int column]
+        {
+            get => grid[row, column];
+            set => grid[row, column] = value;
+        }
+
+        
     }
 }
