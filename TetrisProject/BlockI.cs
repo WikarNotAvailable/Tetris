05668d9bef30f615b3155e33125c13bb5bcda7c7
daniwik662 <daniwik662@student.polsl.pl>
2022-04-01 12:13:57 +0200

    implemented overrided properties for block I

--- TetrisProject/BlockI.cs
+++ TetrisProject/BlockI.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace TetrisProject
 {
-    class BlockI 
-    { 
+    class BlockI : Block
+    {
+        public override int id => 1;
+
+        public override Position[][] positions => new Position[][]
+        {
+           new Position[] {new(0,3),new(0,4),new(0,5),new(0,6)},
+           new Position[] {new(-1,5),new(0,5),new(1,5),new(2,5)},
+           new Position[] {new(1,3),new(1,4),new(1,5),new(1,6)},
+           new Position[] {new(-1,4),new(0,4),new(1,4),new(2,4)}
+        };
     }
 }
