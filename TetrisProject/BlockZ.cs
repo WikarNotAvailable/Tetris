7aac0a1e24dc93bb7101f292984fc4baab95f3f4
WikarNotAvailable <wikar8998@gmail.com>
2022-04-01 13:40:12 +0200

    implemented overrided properties for block Z

--- TetrisProject/BlockZ.cs
+++ TetrisProject/BlockZ.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace TetrisProject
 {
-    class BlockZ
+    class BlockZ : Block
     {
+        public override int id => 7;
+
+        public override Position[][] positions => new Position[][]
+        {
+           new Position[] {new(0,3),new(0,4),new(1,4),new(1,5)},
+           new Position[] {new(1,4),new(1,5),new(0,5),new(2,4)},
+           new Position[] {new(1,3),new(1,4),new(2,4),new(2,5)},
+           new Position[] {new(0,3),new(1,3),new(1,4),new(2,4)}
+        };
     }
 }
--- TetrisProject/MainWindow.xaml.cs
+++ TetrisProject/MainWindow.xaml.cs
@@ -34,7 +34,7 @@ namespace TetrisProject
             
         private readonly Image[,] imgCtrls;
         private GameBoard board = new GameBoard();
-        private Block block = new BlockO(); 
+        private Block block = new BlockZ(); 
 
         private Image[,] SetTetrisCanvas()
         {
