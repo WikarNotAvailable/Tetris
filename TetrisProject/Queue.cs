34c5ebc6c8ebc81be04ec8ff2263c4b2d559e5a0
WikarNotAvailable <wikar8998@gmail.com>
2022-04-18 12:28:42 +0200

    Added Queue class suited to generate new blocks that are moved during the game before placing them

---
+++ TetrisProject/Queue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisProject
+{
+    class Queue
+    {
+        private Random rd;
+        private Block[] blocks;
+        private int DrawID()
+        {
+            return rd.Next(1, 8);
+        }
+        public Queue()
+        {
+            rd = new Random();
+            blocks = new Block[]
+            {
+                new BlockI(),
+                new BlockJ(),
+                new BlockL(),
+                new BlockO(),
+                new BlockS(),
+                new BlockT(),
+                new BlockZ()
+            };
+        }
+        public Block this[int id]
+        {
+            get => blocks[id];
+        }
+    }
+}
