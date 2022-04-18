a326b492840e1cc687be281c9b7c874989930a6b
WikarNotAvailable <wikar8998@gmail.com>
2022-04-18 13:50:36 +0200

    added 2 methods - one to place the tile on the board, second to check if tile is empty

--- TetrisProject/GameBoard.cs
+++ TetrisProject/GameBoard.cs
@@ -28,7 +28,19 @@ namespace TetrisProject
             get => grid[row, column];
             set => grid[row, column] = value;
         }
-
+  
+        public void PlaceTile(int row, int column, int ID)
+        {
+            this[row, column] = ID;
+        }
+        public bool CanMove (int row, int column)
+        {
+            if (this[row, column] == 0)
+                return true;
+            else
+                return false;
+        }
+        
         
     }
 }
