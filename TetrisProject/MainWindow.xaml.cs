1b1c81d3de66eb38adfaa52bafdd24b9b1fa3313
WikarNotAvailable <wikar8998@gmail.com>
2022-05-10 21:41:04 +0200

    finished ranking

--- TetrisProject/MainWindow.xaml.cs
+++ TetrisProject/MainWindow.xaml.cs
@@ -49,7 +49,49 @@ namespace TetrisProject
         private readonly Image[,] imgCtrls;
         private GameHandler handler;
         private string path;
+        private List<User> usersScores;
 
+        private void ReadDatabase() 
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                string[] splittedStrings;
+                string nick;
+                int score;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    splittedStrings = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    nick = splittedStrings[0];
+                    score = Convert.ToInt32(splittedStrings[1]);
+                    usersScores.Add(new User(nick, score));
+                }
+            }
+        }
+        private void SetPlaceholders()
+        {
+            int index = usersScores.Count() - 1;
+            for (int i= index; i<5; i++)
+            {
+                usersScores.Add(new User("Not ranked yet", 0));
+            }
+        }
+        private int CompareUsersByScore(User x, User y)
+        {
+            if (x.score < y.score)
+                return 1;
+            else if (x.score > y.score)
+                return -1;
+            else return 0;
+        }
+        private void AssignDatabaseToRanking()
+        {
+            First.Text = "1. " + usersScores[0].nickname + " " + usersScores[0].score;
+            Second.Text = "2. " +  usersScores[1].nickname + " " + usersScores[1].score;
+            Third.Text = "3. " + usersScores[2].nickname + " " + usersScores[2].score;
+            Fourth.Text = "4. " + usersScores[3].nickname + " " + usersScores[3].score;
+            Fifth.Text = "5. " + usersScores[4].nickname + " " + usersScores[4].score;
+        }
         private Image[,] SetTetrisCanvas()
         {
             Image[,] imgCtrlls = new Image[22,10];
@@ -114,14 +156,23 @@ namespace TetrisProject
         {
             InitializeComponent();
             imgCtrls = SetTetrisCanvas();
+            usersScores = new List<User>();
 
-           path = Directory.GetCurrentDirectory();
-           path += "\\database.txt";
+            path = Directory.GetCurrentDirectory();
+            path += "\\database.txt";
 
             if (!File.Exists(path))
             { 
                 StreamWriter sw = File.CreateText(path); 
-            }              
+            }
+            else
+            {
+                ReadDatabase();
+                usersScores.Sort(CompareUsersByScore);
+                SetPlaceholders();
+                AssignDatabaseToRanking();
+            }
+                
         }
         private void VolumeClick(object sender, RoutedEventArgs e)
         {
@@ -157,6 +208,9 @@ namespace TetrisProject
             if (pattern.IsMatch(Nickname.Text))
             {
                 File.AppendAllText(path, Nickname.Text + " " + handler.ReturnCurrentScore() + "\n");
+                usersScores.Add(new User(Nickname.Text, handler.ReturnCurrentScore()));
+                usersScores.Sort(CompareUsersByScore);
+                AssignDatabaseToRanking();
                 StartingMenu.Visibility = Visibility.Visible;
                 EndGamePanel.Visibility = Visibility.Hidden;
             }
---
+++ TetrisProject/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisProject
+{
+    public struct User
+    {
+        public string nickname { get; }
+        public int score { get; }
+
+        public User (string nick, int achievedScore) 
+        {
+            nickname = nick;
+            score = achievedScore;
+        }
+    }
+}
